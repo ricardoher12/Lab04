b14fb956ff74d020ab4c5b7ac3fab4a48ac5bd70
ricardoher12 <ricardo.hernandezr2@hotmail.com>
2018-09-26 18:27:15 -0600

    Se añaden funciones de prueba

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hola mundo");
+
+const four = 4;
+const amiguis = ["Marioco", "Luisin", "Abuelin", "Marre", "Josesin", "Carpio", "Umaña"];
+var x = 5;
+
+function Sumar(a)
+{
+    var total = a +four;
+    return total;
+}
+
+function MostrarTexto(){
+    return "Hola mis hijos.";
+}
+
+function MostrarAmigos()
+{
+    var amigos = "Mis amigos son: ";
+    for (let index = 0; index < amiguis.length; index++) {
+        amigos = amigos + " " + amiguis[index] +",";
+        
+    }
+
+    return amigos;
+}
+
+console.log(Sumar(5));
+console.log(MostrarTexto());
+console.log(MostrarAmigos());
