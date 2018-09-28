42a5f5fb77a089af7eb97587758ffcd4bff0df2a
ricardoher12 <ricardo.hernandezr2@hotmail.com>
2018-09-28 17:20:12 -0600

    Se sube el metodo para detectar si falta o no un parentesis, llave o corchete

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("hola mundo");
+const {argv, getComando} = require('./yargs');
+
+let cmd = getComando();
+
+let comando = cmd.toString();
+
+
+    var stack = [];
+
+     for (let i = 0; i < comando.length; i++) {
+
+         if(comando.charAt(i) == "{" || comando.charAt(i) == "(" || comando.charAt(i) == "["){
+            stack.push(comando.charAt(i));
+         }
+         
+         if(comando.charAt(i) == "}" || comando.charAt(i) == ")" || comando.charAt(i) == "]"){
+            try {
+
+                let chr = stack.pop();
+                
+            switch(comando.charAt(i))
+            {
+                case "}":
+                if(chr != "{"){
+                    console.log("Cadena invalida se esperaba {");
+                    return;
+                }
+                break;
+
+                case ")":
+                if(chr != "("){
+                    console.log("Cadena invalida se esperaba (");
+                    return;
+                }
+                break;
+
+                case "]":
+                if(chr != "["){
+                    console.log("Cadena invalida se esperaba [");
+                    return;
+                }
+                break;
+            }
+                
+            } catch (error) {
+             console.log("Cadena invalida");
+            }      
+     }
+    }
+
+    if(stack.length != 0)
+    {
+        console.log("Cadena invalida");
+    }
