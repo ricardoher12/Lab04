3161e5b0c0da00cb04b9719e800c84ad98cc3bfa
ricardoher12 <ricardo.hernandezr2@hotmail.com>
2018-09-29 21:19:26 -0600

    Se suben cambios para utilizar libreria http

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-var express = require('express');
-var app = express();
+var http = require('http')
+var finalhandler = require('finalhandler')
+var Router = require('router')
+const PORT = 3000; 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// create the router and server
+var router = new Router()
+var server = http.createServer(function onRequest(req, res) {
+  router(req, res, finalhandler(req, res))
+})
+ 
+// register a route and add all methods
+router.route('/hello/:name')
+  .get(function (req, res) {
+    // this is GET /pet/:id
+    var name = req.params.name;
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ hello: name }))
+  })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-app.get('/hello/:name/', sayHello);
-
-function sayHello(request, response) {
-    var name = request.params.name;
-    response.send(JSON.stringify({"hello": name}));
-  }
+  // register a route and add all methods
+router.route('/')
+.get(function (req, res) {
+  // this is GET /pet/:id
+  var name = req.params.name;
+  res.setHeader('Content-Type', 'application/json')
+  res.end("Welcome")
+})
+ 
+// make our http server listen to connections
+server.listen(PORT);
+console.log("The server is listening on port:" + PORT.toString());
